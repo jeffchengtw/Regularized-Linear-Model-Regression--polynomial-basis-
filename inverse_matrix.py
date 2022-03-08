e46d2271a9c5717f65044828b9c569ff76838da5
jeffchengtw <88997385+jeffchengtw@users.noreply.github.com>
2022-03-09 02:33:00 +0800

    updata

--- inverse_matrix.py
+++ inverse_matrix.py
@@ -1,9 +1,13 @@
 import numpy as np
 
+'''
+formula
+1 / det(A) * adjA
+'''
 def minormatrix(m, row, col):
     return [i[:col]+i[col+1:] for i in (m[:row]+m[row+1:])]
-    
-def determinant(m):
+
+def compute_det(m):
     m_length = len(m)
     if m_length == 2:
         dmt = (m[0][0]*m[1][1]) - (m[0][1]*m[1][0])
@@ -11,13 +15,13 @@ def determinant(m):
         dmt = 0
         for col in range(m_length):
             minor = minormatrix(m,0,col)
-            dmt += ((-1)**col)*m[0][col] * determinant(minor)
+            dmt += ((-1)**col)*m[0][col] * compute_det(minor)
     return dmt
 
 def inverse(m):
     if isinstance(m, np.ndarray):
         m = m.tolist()
-    dmt = determinant(m)
+    dmt = compute_det(m)
     m_length = len(m)
     if m_length == 2:
         molecular = np.array([[m[1][1], -m[0][1]],
@@ -29,7 +33,7 @@ def inverse(m):
             molecular_row = []
             for col in range(m_length):
                 minor = minormatrix(m,row,col)
-                molecular_row.append(((-1)**(row+col)) * determinant(minor))
+                molecular_row.append(((-1)**(row+col)) * compute_det(minor))
             molecular.append(molecular_row)
         mInverse = (np.array(molecular).T) / dmt
     return mInverse
--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from argparse import ArgumentParser
 import numpy as np
 import matplotlib.pyplot as plt
-from model import LSE, Newton
+from method import LSE, Newton
+from utils import *
 
 
 
-def X_polybases(data, N, n=1):
-    X = [np.ones(N)]
-    for j in range(n-1):
-        X.append([data[i]**(j+1) for i in range(N)])
-    return np.vstack(X[::-1]).T
+
 
 
 x_data = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
 y_label = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 
 lambd = 0
-n = 2
+n = 3
 N = int(len(x_data))
 X = X_polybases(x_data, N, n=n)
 y = [y_label[i] for i in range(N)]
 
 if(True):
     model = LSE()
-    model.fit(X,y, lambd=lambd)
+    model.fit(X,y, lmbd=lambd)
     model.predict(X)
-    model.show_report(X,y)
-    model.plot_res(X,y)
+    model.visualize(X,y)
 
     model = Newton()
     model.fit(X,y)
-    model.show_report()
-    model.plot_res(X,y)
+    model.predict(X)
+    model.visualize(X,y)
+
+    
+    
